31e385590c7a53ea698bc18f960b2841a876dda9
leonardo_mora_obando <lmoraobando@gmail.com>
2021-05-03 17:41:28 -0600

    add new example

--- DependencyInjectionDemo/Program.cs
+++ DependencyInjectionDemo/Program.cs
@@ -8,8 +8,15 @@ namespace DependencyInjectionDemo
         static void Main(string[] args)
         {
             NotificateToDB notificateToDB = new NotificateToDB();
+            NotificatorToEmail notificatorToEmail = new NotificatorToEmail();
+
+
             NotificationOperator notificationOperator = new NotificationOperator(notificateToDB);
             notificationOperator.SendNotification("Hola mundo");
+
+            NotificationOperator notificationOperator1 = new NotificationOperator(notificatorToEmail);
+            notificationOperator1.SendNotification("Hola mundo 2");
+
 
 
         }
